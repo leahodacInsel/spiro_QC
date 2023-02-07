9e9c72e84738d9998626b0eba28c76d2c6594115
leahodacInsel <lea.hodac@insel.ch>
2023-02-07 17:01:16 +0100

    test commit for proxy setting

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 from rawdata import get_raw_vectors, plot_raw_curves, get_parameters
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
 
     print('hello')
     # Data path and name
+    path = "C:/Users/I0337516/Desktop/"
+    nameFile = 'Export3-Trials.xml'
+
+    # build vectors Volume-Time and Flow-Volume
+    [vol_VT, time_VT, flow_FV, vol_FV, nameFile] = get_raw_vectors(path, nameFile)
+
+    # plot curves
+    plot_raw_curves(vol_VT, time_VT, flow_FV, vol_FV, nameFile)
+
+    # get parameters from SentrySuite output
+    get_parameters(path, nameFile)
+
 '''
     # path = 'L:\KKM_LuFu\OfficeData\Biomedical Engineers\Lea\project QC\spirometry_qualityControl\SentrySuite\...
     # XMLExport_Overview\ExamplePlots'
@@ -17,13 +30,5 @@ if __name__ == '__main__':
 
     path = "L:\KKM_LuFu\OfficeData\Biomedical Engineers\Lea"
     nameFile = "Eckert151122.xml"
-
-    # build vectors Volume-Time and Flow-Volume
-    [vol_VT, time_VT, flow_FV, vol_FV, nameFile] = get_raw_vectors(path, nameFile)
-
-    # plot curves
-    plot_raw_curves(vol_VT, time_VT, flow_FV, vol_FV, nameFile)
-
-    # get parameters from SentrySuite output
-    get_parameters(path, nameFile)
 '''
+
